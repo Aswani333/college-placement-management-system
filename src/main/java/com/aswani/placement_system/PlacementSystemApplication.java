2b44837ba97c76f7abbfd11136fdd794870ebcf4
Aswani333 <tekuriaswani333@gmail.com>
2026-08-06 10:24:08 +0530

    Added Spring Boot project setup

---
+++ src/main/java/com/aswani/placement_system/PlacementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aswani.placement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementSystemApplication.class, args);
+	}
+
+}
